1754e29d1816a8b5ab9b1ca0c24e291bd2112158
Loganathanskt <loganathanskt@gmail.com>
2022-03-29 19:27:11 +0530

    Message

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensOmr() {
+		System.out.println("greensOmr");
+	} 
+	private void greensAdayar() {
+		System.out.println("greensAdayar");
+	}
+	
+	private void greensTambaram() {
+		System.out.println("greensTambaram");
+	}
+	private void greensVelacherry() {
+		System.out.println("greensVelacherry");
+	}
+	private void greensAnnaNagar() {
+		System.out.println("greensAnnaNagar");
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GreensTech ernTech = new GreensTech();
+		ernTech.greensAdayar();
+		ernTech.greensAnnaNagar();
+		ernTech.greensOmr();
+		ernTech.greensTambaram();
+		ernTech.greensVelacherry();
+
+}
+}
